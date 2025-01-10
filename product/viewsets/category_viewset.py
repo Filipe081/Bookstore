8fe3ecaafc0e477fbddba879d7c0f03ba65d3c88
Filipe081 <filipejoserds123@gmail.com>
2025-01-10 18:42:26 -0300

    alterando alguns arquivos

--- product/viewsets/category_viewset.py
+++ product/viewsets/category_viewset.py
@@ -1,7 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-
 from rest_framework.viewsets import ModelViewSet
 
 from product.models import Category
@@ -12,4 +8,4 @@ class CategoryViewSet(ModelViewSet):
     serializer_class = CategorySerializer
 
     def get_queryset(self):
-        return Category.objects.all().order_by("id")
+        return Category.objects.all().order_by('id')
